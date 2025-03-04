48aebc67bcf7349cd8e0f7af778fd94f8d2245c1
Igor Popov <ipopov4422@gmail.com>
2025-03-04 17:58:18 +0000

    Change Grid to Flex

--- app/components/IPAddressInfo.tsx
+++ app/components/IPAddressInfo.tsx
@@ -1,22 +1,22 @@
-import { IPData } from "~/types/ip-data"
+import { IPData } from '~/types/ip-data'
 
 interface IPAddressInfoProps {
-  data: IPData | null
+  data?: IPData
   isLoading: boolean
 }
 
 const IPAddressInfo = ({ data, isLoading }: IPAddressInfoProps) => {
   return (
-    <div className="min-w-[80%] bg-white rounded-lg shadow-md p-6 lg:p-8 max-w-6xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 text-center md:text-left">
+    <div className="min-w-[80%] bg-white rounded-lg shadow-md p-6 lg:p-8 max-w-6xl flex flex-wrap gap-6 text-center md:text-left">
       {isLoading ? (
-        <div className="col-span-full text-center py-4">
+        <div className="w-full text-center py-4">
           <div className="animate-pulse flex justify-center">
             <div className="h-4 bg-gray-200 rounded w-32"></div>
           </div>
         </div>
       ) : data ? (
-        <>
-          <div className="lg:border-r lg:border-gray-300 lg:pr-6">
+        <div className="w-full flex flex-col md:flex-row text-center md:text-left gap-x-6">
+          <div className="w-full md:flex-1 px-6 md:border-r md:border-gray-300">
             <h2 className="text-xs text-gray-500 uppercase tracking-wider font-bold mb-1">
               IP Address
             </h2>
@@ -25,7 +25,7 @@ const IPAddressInfo = ({ data, isLoading }: IPAddressInfoProps) => {
             </p>
           </div>
 
-          <div className="lg:border-r lg:border-gray-300 lg:px-6">
+          <div className="w-full md:flex-1 px-6 md:border-r md:border-gray-300 sm:pt-6">
             <h2 className="text-xs text-gray-500 uppercase tracking-wider font-bold mb-1">
               Location
             </h2>
@@ -36,7 +36,7 @@ const IPAddressInfo = ({ data, isLoading }: IPAddressInfoProps) => {
             </p>
           </div>
 
-          <div className="lg:border-r lg:border-gray-300 lg:px-6">
+          <div className="w-full md:flex-1 px-6 md:border-r md:border-gray-300 sm:pt-6">
             <h2 className="text-xs text-gray-500 uppercase tracking-wider font-bold mb-1">
               Timezone
             </h2>
@@ -45,7 +45,7 @@ const IPAddressInfo = ({ data, isLoading }: IPAddressInfoProps) => {
             </p>
           </div>
 
-          <div className="lg:pl-6">
+          <div className="w-full md:flex-1 px-6 sm:pt-6">
             <h2 className="text-xs text-gray-500 uppercase tracking-wider font-bold mb-1">
               ISP
             </h2>
@@ -53,9 +53,9 @@ const IPAddressInfo = ({ data, isLoading }: IPAddressInfoProps) => {
               {data.isp}
             </p>
           </div>
-        </>
+        </div>
       ) : (
-        <div className="col-span-full text-center py-4">
+        <div className="w-full text-center py-4">
           <p className="text-gray-500">
             Enter an IP address or domain to see details
           </p>
