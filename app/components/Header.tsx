8b02b880446968b8927810b2f16d4aca5b80d63e
Igor Popov <ipopov4422@gmail.com>
2025-03-04 18:06:26 +0000

    Add dynamic top position calculation for IPAddressInfo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,20 +1,38 @@
+import { useEffect, useRef, useState } from 'react'
 import IPAddressForm from './IPAddressForm'
 import IPAddressInfo from './IPAddressInfo'
+import { IPData } from '~/types/ip-data'
 
 interface HeaderProps {
   onSearch: (query: string) => void
+  data?: IPData
   isLoading: boolean
 }
 
-const Header = ({ onSearch, isLoading }: HeaderProps) => {
+const Header = ({ onSearch, data, isLoading }: HeaderProps) => {
+  const formRef = useRef<HTMLDivElement>(null)
+  const [infoTop, setInfoTop] = useState(0)
+
+  useEffect(() => {
+    if (formRef.current) {
+      // Calculate top position for IPAddressInfo dynamically
+      const formHeight =
+        formRef?.current?.offsetHeight + formRef?.current?.offsetTop
+      setInfoTop(formHeight + 24) // 24px is a margin for spacing
+    }
+  }, [])
+
   return (
     <header className="relative pt-6 pb-32 text-center bg-cover bg-center">
       <h1 className="text-2xl font-bold text-white mb-6">IP Address Tracker</h1>
-      <div className="max-w-xl mx-auto px-6">
+      <div ref={formRef} className="max-w-xl mx-auto px-6">
         <IPAddressForm onSearch={onSearch} isLoading={isLoading} />
       </div>
-      <div className="absolute top-[200px] left-0 w-full px-10 flex justify-center z-10">
-        <IPAddressInfo data={null} isLoading={isLoading} />
+      <div
+        className="absolute left-0 w-full px-10 flex justify-center z-10"
+        style={{ top: `${infoTop}px` }}
+      >
+        <IPAddressInfo data={data} isLoading={isLoading} />
       </div>
     </header>
   )
